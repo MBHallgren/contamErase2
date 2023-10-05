63384aa6d1266e67148125292a7ca67abc1a4094
MBHallgren <s153002@student.dtu.dk>
2023-10-05 09:21:56 +0200

    test17

--- nanodecon/decontaminate_illumina.py
+++ nanodecon/decontaminate_illumina.py
@@ -68,7 +68,7 @@ def produce_species_specific_kma_db(species, fsa_file, scheme_file, output):
     produce_species_fsa_file(fsa_file, gene_set, output)
 
 def produce_species_fsa_file(fsa_file, gene_set, output):
-    with open(output + 'specie.fsa', 'w') as outfile:
+    with open(output + '/specie.fsa', 'w') as outfile:
         with open(fsa_file, 'r') as f:
             write_sequence = False  # A flag to indicate whether the sequence should be written to the output
             for line in f:
