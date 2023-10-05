22dc8b40bbb28a43da2c76b75bf1c9a95ea135b6
MBHallgren <s153002@student.dtu.dk>
2023-10-05 11:49:46 +0200

    consensus allee17

--- nanodecon/decontaminate_illumina.py
+++ nanodecon/decontaminate_illumina.py
@@ -96,9 +96,8 @@ def build_consensus_dict(arguments, res_file, mat_file):
             line = line.strip()
             if line.startswith('#'):
                 print(line)
-                allele = line[1:].split('_')[0]
-                gene =  line[1:][0]
-                print (gene, allele)
+                allele = line[1:]
+                print (allele)
                 if gene in correct_size_alleles:
                     print ('found correct size allele')
                     correct_size_flag = True
