8a98057e2fcb84143cbe61d67363b6a5c33e3cbe
MBHallgren <s153002@student.dtu.dk>
2023-10-05 09:08:32 +0200

    test7

--- nanodecon/decontaminate_illumina.py
+++ nanodecon/decontaminate_illumina.py
@@ -57,6 +57,7 @@ def produce_species_specific_kma_db(species, fsa_file, scheme_file):
             if line.startswith('rST'):
                 headers = line.strip().split('\t')[1:54]
             else:
+                print (line.strip().split('\t'))
                 print (line.strip().split('\t')[56])
                 break
     #print (headers)
