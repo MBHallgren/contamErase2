a594c729dbe137da6d5e60bd5370000714048f85
MBHallgren <s153002@student.dtu.dk>
2023-10-05 11:50:12 +0200

    consensus allee18

--- nanodecon/decontaminate_illumina.py
+++ nanodecon/decontaminate_illumina.py
@@ -98,6 +98,7 @@ def build_consensus_dict(arguments, res_file, mat_file):
                 print(line)
                 allele = line[1:]
                 print (allele)
+                """
                 if gene in correct_size_alleles:
                     print ('found correct size allele')
                     correct_size_flag = True
@@ -110,6 +111,7 @@ def build_consensus_dict(arguments, res_file, mat_file):
                     for i in range(len(line)):
                         consensus_dict[allele][index][i] += int(line[i])
                     index += 1
+                    """
     #for item in consensus_dict:
     #    print (item, consensus_dict[item])
 
