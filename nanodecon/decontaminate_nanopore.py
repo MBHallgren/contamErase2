a192cff1b66da239c1134ae05aad5710c246948e
MBHallgren <s153002@student.dtu.dk>
2023-10-09 10:10:14 +0200

    t17

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -49,9 +49,9 @@ def nanopore_decontamination(arguments):
                                  arguments.output + '/specie.fsa')
     print ('mutations:')
     for item in mutation_dict:
-        print (item, mutation_dict[item])
-        #if 'BACT000040' in item:
-        #    print(mutation_dict[item])
+        #print (item, mutation_dict[item])
+        if 'BACT000040' in item[1]:
+            print(mutation_dict[item])
     print ('confirmed:')
     for item in confirmed_alleles:
         if 'BACT000040' in item:
