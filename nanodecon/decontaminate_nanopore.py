2f8391dbc339698e9a3a05a315273b06567a372e
MBHallgren <s153002@student.dtu.dk>
2023-10-10 12:17:49 +0200

    t74

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -42,7 +42,9 @@ def nanopore_decontamination(arguments):
                                                        headers,
                                                        arguments,
                                                        top_alleles)
-
+    for allele in mutation_position_dict:
+        print (allele, mutation_position_dict[allele])
+    sys.exit()
     reads_mutation_dict = parse_sam_and_find_mutations(arguments.output + '/rmlst_alignment.sam',
                                  arguments.output + '/specie.fsa',
                                                  allele_pair_dict)
