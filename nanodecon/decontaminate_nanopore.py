275a2869c67b919145e0f0af485549549ccb6b53
MBHallgren <s153002@student.dtu.dk>
2023-10-10 12:37:38 +0200

    t834

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -64,6 +64,14 @@ def determine_mutation_sets(reads_mutation_dict, mutation_position_dict):
         gene_mutation_set = set(mutation_position_dict[reference][1])
         if read_mutation_set != set():
             print (read_mutation_set, gene_mutation_set)
+            if read_mutation_set.issubset(gene_mutation_set):
+                if reference + '_' + str(read_mutation_set) not in mutation_count_dict:
+                    mutation_count_dict[reference + '_' + str(read_mutation_set)] = 1
+                else:
+                    mutation_count_dict[reference + '_' + str(read_mutation_set)] += 1
+
+    for item in mutation_count_dict:
+        print (item, mutation_count_dict[item])
 
 def derive_rmlst_scheme_candidates(confirmed_alleles, rmlst_scheme_file):
     pass
