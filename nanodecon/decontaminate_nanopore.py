f588594e58db2bc0b6ce9fb2e7db2211faf996e1
MBHallgren <s153002@student.dtu.dk>
2023-10-23 10:12:50 -0300

    matrix33

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -85,9 +85,12 @@ def co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, gene_scor
     reads_mutation_dict = parse_sam_and_find_mutations(arguments.output + '/rmlst_alignment.sam',
                                                        arguments.output + '/specie.fsa',
                                                        allele_pair_dict)
+    t = 0
     for read in reads_mutation_dict:
         if 'BACT000038' in read:
             print (read, reads_mutation_dict[read])
+            t += 1
+    print (t)
     sys.exit()
 
     for allele in confirmed_mutation_dict:
