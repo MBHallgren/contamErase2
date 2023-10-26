868c790af16906ef3c3bc6fc8b74d990f44433c1
MBHallgren <s153002@student.dtu.dk>
2023-10-26 09:23:14 -0300

    matrix83

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -188,8 +188,8 @@ def validate_mutations(arguments, mutation_position_dict, gene_score_dict, fsa_f
     return_dict = dict()
     for item in mutation_position_dict:
         return_dict[item] = [0, [], [], 0]
-        for i in range(len(mutation_position_dict[item][1])):
-            if mutation_position_dict[item][1][i] in mutations_found_in_rmlst_genes[item.split('_')[0]]:
+        for i in range(len(mutation_position_dict[item][0])):
+            if mutation_position_dict[item][0][i] in mutations_found_in_rmlst_genes[item.split('_')[0]]:
                 return_dict[item][1].append(mutation_position_dict[item][1][i])
                 return_dict[item][2].append(mutation_position_dict[item][2][i])
                 return_dict[item][3] = mutation_position_dict[item][3]
