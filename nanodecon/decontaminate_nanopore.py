ae35d26afbdb38e3048107f0755ff3aeaedcb84a
MBHallgren <s153002@student.dtu.dk>
2023-11-17 12:37:11 +0100

    prune60

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -381,15 +381,13 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
             print ("allele:", allele)
             print("Mutation names:", mutation_list)
             print ("Depth:", depth_list)
-            #average_depth = sum(confirmed_mutation_dict[allele][1]) / len(confirmed_mutation_dict[allele][1])
+            average_depth = sum(confirmed_mutation_dict[allele][1]) / len(confirmed_mutation_dict[allele][1])
             #positional_depth = sum(consensus_dict[allele][0][0]) / len(consensus_dict[allele][0][0])
-            total_gene_depth = 0
-            for i in range(len(consensus_dict[allele][0])):
-                total_gene_depth += sum(consensus_dict[allele][0][i])
-            average_depth = total_gene_depth / len(consensus_dict[allele][0])
-            print ("Average depth:", average_depth)
-            print ("Average depth:", average_depth)
-            sys.exit()
+            #total_gene_depth = 0
+            #for i in range(len(consensus_dict[allele][0])):
+            #    total_gene_depth += sum(consensus_dict[allele][0][i])
+            #average_depth = total_gene_depth / len(consensus_dict[allele][0])
+            #threshold = average_depth * arguments.mrd * arguments.coc
             print ("Threshold:", average_depth * 0.5) #Here, TBD look at threshold. Is more 0.5 really fine? Or should we do something similar to the benchmarking script
             for i, row in enumerate(co_occurrence_matrix):
                 mutation_name = mutation_list[i]
@@ -398,6 +396,7 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
             co_occurence_matrix_dict[allele] = [co_occurrence_matrix, mutation_list]
 
 
+    #TBD continue here.
     adjusted_mutation_dict = {}
     for allele in confirmed_mutation_dict:
         if allele in co_occurence_matrix_dict:
