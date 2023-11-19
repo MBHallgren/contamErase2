a16f26073d7007d9242168f3c3fdb0c1f08e5040
MBHallgren <s153002@student.dtu.dk>
2023-11-19 20:47:49 +0100

    prune65

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -421,11 +421,13 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
                 if threshold < 3:
                     threshold = 3
                 if allele == 'BACT000064_31':
+                    print ("mutation:", mutation)
                     print ("threshold:", threshold)
                     print ("position_depth:", position_depth)
-                    print ("mutation:", mutation)
                     print ("proxi_mutations:", proxi_mutations)
                     print ("biological_existance:", biological_existance)
+                    print (arguments.mrd)
+                    print (arguments.coc)
                 for number_of_co_occurences in row:
                     if float(number_of_co_occurences) >= float(threshold): #Positive co-occurence
                         adjusted_mutation_dict[allele][0].append(confirmed_mutation_dict[allele][0][i])
