435521140331fe5709b44c6a8f30fa804c816945
MBHallgren <s153002@student.dtu.dk>
2023-11-20 14:35:11 +0100

    test16

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -367,9 +367,9 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
                                 co_occurrence_matrix[mutation2][mutation1] += 1
 
             # Print the co-occurrence matrix with mutation names
-            print ("allele:", allele)
-            print("Mutation names:", mutation_list)
-            print ("Depth:", depth_list)
+            #print ("allele:", allele)
+            #print("Mutation names:", mutation_list)
+            #print ("Depth:", depth_list)
             average_depth = sum(confirmed_mutation_dict[allele][1]) / len(confirmed_mutation_dict[allele][1])
             #positional_depth = sum(consensus_dict[allele][0][0]) / len(consensus_dict[allele][0][0])
             #total_gene_depth = 0
@@ -378,9 +378,9 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
             #average_depth = total_gene_depth / len(consensus_dict[allele][0])
             #threshold = average_depth * arguments.mrd * arguments.coc
             #print ("Threshold:", average_depth * 0.5) #Here, TBD look at threshold. Is more 0.5 really fine? Or should we do something similar to the benchmarking script
-            for i, row in enumerate(co_occurrence_matrix):
-                mutation_name = mutation_list[i]
-                print(f"{mutation_name}: {row} {check_single_mutation_exisistance(bio_validation_dict, allele, mutation_name)}")
+            #for i, row in enumerate(co_occurrence_matrix):
+            #    mutation_name = mutation_list[i]
+            #    print(f"{mutation_name}: {row} {check_single_mutation_exisistance(bio_validation_dict, allele, mutation_name)}")
 
             co_occurence_matrix_dict[allele] = [co_occurrence_matrix, mutation_list]
 
@@ -433,7 +433,7 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
                 #relative_depth = confirmed_mutation_dict[allele][1][0] / total_depth
                 #if relative_depth >= arguments.mrd:
                 if confirmed_mutation_dict[allele][1][0] >= position_depth * arguments.mrd:
-                    print ('Added single mutation_2: ', allele, mutation)
+                    #print ('Added single mutation_2: ', allele, mutation)
                     adjusted_mutation_dict[allele][0].append(confirmed_mutation_dict[allele][0][0])
                     adjusted_mutation_dict[allele][1].append(confirmed_mutation_dict[allele][1][0])
     return adjusted_mutation_dict
