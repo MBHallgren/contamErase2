2caa2897f3eb5c8d8857bb566797ba9411703aaf
MBHallgren <s153002@student.dtu.dk>
2023-11-20 09:36:52 +0100

    n12

--- nanodecon/decontaminate_nanopore.py
+++ nanodecon/decontaminate_nanopore.py
@@ -14,11 +14,11 @@ from nanodecon.nanopore_mutations import create_mutation_vector
 from nanodecon.nanopore_mutations import identify_mutations
 
 def nanopore_decontamination(arguments):
-    os.system('mkdir ' + arguments.output)
-    kma.KMARunner(arguments.nanopore,
-                  arguments.output + "/bacteria_alignment",
-                  arguments.db_dir + "/bac_db",
-                  "-mem_mode -1t1 -t {} -ID 10 -ont -eq 14 -mct 0.5".format(arguments.threads)).run()
+    #os.system('mkdir ' + arguments.output)
+    #kma.KMARunner(arguments.nanopore,
+    #              arguments.output + "/bacteria_alignment",
+    #              arguments.db_dir + "/bac_db",
+    #              "-mem_mode -1t1 -t {} -ID 10 -ont -eq 14 -mct 0.5".format(arguments.threads)).run()
 
     total_bacteria_aligning_bases = util.number_of_bases_in_file(arguments.output + "/bacteria_alignment.fsa")
     primary, candidate_dict = drive_bacteria_results(arguments, total_bacteria_aligning_bases)
@@ -30,27 +30,27 @@ def nanopore_decontamination(arguments):
                                     '/home/people/malhal/contamErase_db/rmlst_scheme.txt',
                                     arguments.output)
 
-    kma.KMARunner(arguments.nanopore,
-                  arguments.output + "/initial_rmlst_alignment",
-                  arguments.output + '/specie_db',
-                  "-t {} -ID 10 -ont -md 1.5 -matrix -eq 14 -mct 0.5".format(arguments.threads, arguments.output)).run()
+    #kma.KMARunner(arguments.nanopore,
+    #              arguments.output + "/initial_rmlst_alignment",
+    #              arguments.output + '/specie_db',
+    #              "-t {} -ID 10 -ont -md 1.5 -matrix -eq 14 -mct 0.5".format(arguments.threads, arguments.output)).run()
 
-    os.system('gunzip ' + arguments.output + '/initial_rmlst_alignment.frag.gz')
+    #os.system('gunzip ' + arguments.output + '/initial_rmlst_alignment.frag.gz')
 
-    extract_mapped_rmlst_read(arguments.output, arguments.nanopore)
+    #extract_mapped_rmlst_read(arguments.output, arguments.nanopore)
 
     #os.system('cat {}/rmlst_reads.fastq | NanoFilt -q 14 -l 500 > {}/trimmed_rmlst_reads.fastq'.format(arguments.output, arguments.output))
 
-    index_top_hits_db(arguments.output)
+    #index_top_hits_db(arguments.output)
 
     arguments.nanopore = arguments.output + '/rmlst_reads.fastq'
     #
 
-    kma.KMARunner(arguments.nanopore,
-                  arguments.output + "/rmlst_alignment",
-                  arguments.output + '/top_hits_db',
-                  "-t {} -ID 10 -ont -md 1.5 -matrix -eq 14 -mct 0.5 -sam 2096> {}/rmlst_alignment.sam".format(arguments.threads, arguments.output)).run()
-    os.system('gunzip ' + arguments.output + '/rmlst_alignment.mat.gz')
+    #kma.KMARunner(arguments.nanopore,
+    #              arguments.output + "/rmlst_alignment",
+    #              arguments.output + '/top_hits_db',
+    #              "-t {} -ID 10 -ont -md 1.5 -matrix -eq 14 -mct 0.5 -sam 2096> {}/rmlst_alignment.sam".format(arguments.threads, arguments.output)).run()
+    #os.system('gunzip ' + arguments.output + '/rmlst_alignment.mat.gz')
 
     consensus_dict = build_consensus_dict(arguments.output + '/rmlst_alignment.res',
                                           arguments.output + '/rmlst_alignment.mat')
@@ -63,19 +63,19 @@ def nanopore_decontamination(arguments):
 
     #confirmed_mutation_dict = validate_mutations(consensus_dict, arguments.output + '/specie.fsa', confirmed_mutation_dict)
 
-    for item in co_occuring_mutations:
-        print(item, co_occuring_mutations[item])
+    for item in confirmed_mutation_dict:
+        print(item, confirmed_mutation_dict[item])
 
     confirmed_mutation_dict, co_occuring_mutations = upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, consensus_dict, read_positions_blacklisted_dict, bio_validation_dict)
 
-    for item in co_occuring_mutations:
-        print (item, co_occuring_mutations[item])
+    for item in confirmed_mutation_dict:
+        print (item, confirmed_mutation_dict[item])
 
 
     confirmed_mutation_dict, co_occuring_mutations = upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, consensus_dict,
                                                                    read_positions_blacklisted_dict, bio_validation_dict)
-    for item in co_occuring_mutations:
-        print(item, co_occuring_mutations[item])
+    for item in confirmed_mutation_dict:
+        print(item, confirmed_mutation_dict[item])
 
     format_output(confirmed_mutation_dict, consensus_dict, bio_validation_dict)
 
