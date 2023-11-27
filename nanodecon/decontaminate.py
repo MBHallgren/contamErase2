f444cb25d70fd22ac467a6fd24b6c408850acee7
MBHallgren <s153002@student.dtu.dk>
2023-11-27 11:30:54 +0100

    db3

--- nanodecon/decontaminate.py
+++ nanodecon/decontaminate.py
@@ -3,7 +3,6 @@ import sys
 import logging
 
 from nanodecon.decontaminate_nanopore import nanopore_decontamination
-from nanodecon.decontaminate_illumina import illumina_decontamination
 
 def decontaminate(arguments):
     set_up_output_and_check_input(arguments)
@@ -15,11 +14,6 @@ def decontaminate(arguments):
 def set_up_output_and_check_input(arguments):
     if not os.path.exists(arguments.output):
         os.makedirs(arguments.output)
-
-    if arguments.illumina != None:
-        if not os.path.isfile(arguments.illumina[0]):
-            print ('Input file does not exist')
-            sys.exit()
 
     if arguments.nanopore != None:
         if not os.path.isfile(arguments.nanopore):
