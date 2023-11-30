88028b73c5322990f448b11a0c1165282612cd3f
MBHallgren <s153002@student.dtu.dk>
2023-11-30 19:15:21 +0100

    format2

--- nanomgt/paper_scripts/rmlst_wrapper.py
+++ nanomgt/paper_scripts/rmlst_wrapper.py
@@ -8,8 +8,8 @@ path = '/home/people/malhal/papers/rmlst/data/mix/subsets'
 #path = '/home/people/malhal/contamErase/benchmarking/confindr/data/nanopore/'
 for rate in bf_rates:
     for i in range(1, 11, 1):
-        cmd ='/home/people/malhal/contamErase2/bin/nanomgt --nanopore {}/{}/{}_intra.fastq --o {}/{} --db_dir /home/people/malhal/contamErase_db/ --threads 4 --mrd 0.0{} --bp 2 --pp 2 > {}/{}_{}.csv'\
+        cmd ='/home/people/malhal/contamErase2/bin/nanomgt --nanopore {}/{}/{}_intra.fastq --o {}/{} --db_dir /home/people/malhal/contamErase_db/ --threads 4 --mrd 0.0{} > {}/{}_{}.csv'\
             .format(path, i, i, rate, i, rate, rate, i, 'results')
-        #cmd ='/home/people/malhal/contamErase2/bin/nanomgt --nanopore {}/{}/intra.fastq --o {}/{} --db_dir /home/people/malhal/contamErase_db/ --threads 4 --mrd 0.0{} --bp 2 --pp 2 > {}/{}_{}.csv'\
+        #cmd ='/home/people/malhal/contamErase2/bin/nanomgt --nanopore {}/{}/intra.fastq --o {}/{} --db_dir /home/people/malhal/contamErase_db/ --threads 4 --mrd 0.0{} > {}/{}_{}.csv'\
         #    .format(path, i, rate, i, rate, rate, i, 'results')
         os.system(cmd)
