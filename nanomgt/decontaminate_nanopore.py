5f80f924b95d72b10db76bfbd6ac436ac447ab4e
MBHallgren <s153002@student.dtu.dk>
2023-12-04 11:46:24 +0100

    push5

--- nanomgt/decontaminate_nanopore.py
+++ nanomgt/decontaminate_nanopore.py
@@ -96,8 +96,8 @@ def co_occurrence_until_convergence(arguments, confirmed_mutation_dict, consensu
         print ('Interations,Mutations', file=convergence_file)
         # Iterate until no new mutations are found
         while True:
-            arguments.cor = arguments.cor + (original_cor * 0.25) #increase of 25% per iteration
-            arguments.dp = arguments.dp + (original_dp * 0.25) #increase of 25% per iteration
+            arguments.cor = arguments.cor + (original_cor * 0.20) #increase of 25% per iteration
+            arguments.dp = arguments.dp + (original_dp * 0.20) #increase of 25% per iteration
             confirmed_mutation_dict, co_occurrence_tmp_dict = upper_co_occuring_mutations_in_reads(
                 arguments,
                 confirmed_mutation_dict,
